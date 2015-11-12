17680e3ab7fa6881bbb51a9e9f4d7c7c3c190b2c
Kyle Gosen <gosenkyl@msu.edu>
2015-11-12 17:04:54 -0500

    Toolbar

--- app/src/main/java/com/sportsschedule/gosenk/sportsscheduleandroid/teams/TeamsActivity.java
+++ app/src/main/java/com/sportsschedule/gosenk/sportsscheduleandroid/teams/TeamsActivity.java
@@ -1,9 +1,14 @@
 package com.sportsschedule.gosenk.sportsscheduleandroid.teams;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.sportsschedule.gosenk.sportsscheduleandroid.teams.fragment.CollectionPagerAdapter;
@@ -25,8 +30,7 @@ public class TeamsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.teams_toolbar);
-        setSupportActionBar(toolbar);
+        setToolBar();
 
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(new CollectionPagerAdapter(getSupportFragmentManager()));
@@ -34,6 +38,41 @@ public class TeamsActivity extends AppCompatActivity {
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabs.setViewPager(pager);
 
+    }
+
+    private void setToolBar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.teams_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemId = item.getItemId();
+        String buttonName = null;
+
+        switch (itemId){
+            case R.id.action_search:
+                buttonName = "Search";
+                break;
+            case R.id.action_help:
+                buttonName = "Help";
+                break;
+            case R.id.action_settings:
+                buttonName = "Settings";
+                break;
+        }
+
+        Toast.makeText(getApplicationContext(), "Butotn " + buttonName, Toast.LENGTH_SHORT).show();
+        return true;
     }
 
 }
